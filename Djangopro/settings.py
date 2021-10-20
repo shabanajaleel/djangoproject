cfd6cc127d3ce5c5ed95d7560b996a7db1a84eec
shabanajaleel <shabanac71@gmail.com>
2021-10-20 10:48:34 +0530

    modified settings.py

--- Djangopro/settings.py
+++ Djangopro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_%@x+-n#7*gw$a=pz-%+i&z!9yvv(jycxl3v4^ari4p_0j_p@@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
